6cb8d281a43386d4fcb270ea9950412e5708a8ba
Iona <ionabrabender@gmail.com>
2020-04-26 16:24:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def my_hash_creator(key, value)
   hash = {key => value}
 end
@@ -7,6 +9,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
+  binding.pry
   if hash[key]
     hash[key] += 1 
   else
